81a59bc2ec87fcf7f1ae72a652e16c4e5229eeff
12blackpearls <12blackpearls@gmail.com>
2017-03-18 22:55:29 +0700

    Add Pandas, Numpy, and some configs

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-import csv 
+import pandas as pd
 
-bag_of_words = []
-with open('tdm.csv') as csvfile:
-    for line in csvfile:
-        bag_of_words.append(line.strip().split(','))
+test = "aku benci"
+test = test.split(' ')
+df = pd.read_csv('dataset/testmnb.csv', sep=',')
+print(df.values)   # print value of df
+#print(df.iloc[0][0])   #test print of a value
+#print(df.columns.values)
+count_class = len(pd.unique(df[['class']].values.ravel()))     # count how many unique value in column(s) -> to count how many class 
+classes = pd.unique(df[['class']].values.ravel())
+each_class = pd.value_counts(df[['class']].values.ravel())     # count each value of column(s) 
 
-print(bag_of_words[1][1])
+for clas in classes:
+    for word in test: 
+        print(df.iloc[0][word])
